6ea2b45c8918c6a14be5cc013e2691b3f0bd6090
William Skene <william.skene@outlook.com>
2024-08-12 16:08:06 +1000

    Implement remaining hand valuations

--- src/poker.rs
+++ src/poker.rs
@@ -134,6 +134,23 @@ impl GameEngine<PokerState, PokerAction> for PokerEngine<'_> {
     }
 }
 
+fn get_rank_counts(cards: &Vec<Card>) -> HashMap<Rank, i64> {
+    cards
+        .iter()
+        .fold(HashMap::<Rank, i64>::new(), |mut acc, x| {
+            *acc.entry(x.rank).or_insert(0) += 1;
+            acc
+        })
+}
+
+fn get_highest_n_of_kind(cards: &Vec<Card>, n: i64) -> Rank {
+    let rank_counts = get_rank_counts(cards);
+    rank_counts.iter().fold(
+        Rank::Null,
+        |acc, (a, b)| if b == &n { acc.max(*a) } else { acc },
+    )
+}
+
 fn straight_flush_value(cards: &Vec<Card>) -> i64 {
     if cards.len() < 5 {
         return -1;
@@ -167,21 +184,22 @@ fn straight_flush_value(cards: &Vec<Card>) -> i64 {
     -1
 }
 
-fn get_rank_counts(cards: &Vec<Card>) -> HashMap<Rank, i64> {
-    cards
+fn quads_value(cards: &Vec<Card>) -> i64 {
+    let values: HashMap<Rank, i64> = get_rank_counts(cards);
+    let quads = get_highest_n_of_kind(cards, 4);
+    let top_card: Rank = cards
+        .clone()
         .iter()
-        .fold(HashMap::<Rank, i64>::new(), |mut acc, x| {
-            *acc.entry(x.rank).or_insert(0) += 1;
-            acc
-        })
-}
+        .filter(|a| a.rank != quads)
+        .max_by(|a, b| a.rank.partial_cmp(&b.rank).unwrap())
+        .unwrap()
+        .rank;
 
-fn get_highest_n_of_kind(cards: &Vec<Card>, n: i64) -> Rank {
-    let rank_counts = get_rank_counts(cards);
-    rank_counts.iter().fold(
-        Rank::Null,
-        |acc, (a, b)| if b == &n { acc.max(*a) } else { acc },
-    )
+    if quads == Rank::Null {
+        -1
+    } else {
+        (quads as i64) * 13 + top_card as i64
+    }
 }
 
 fn full_house_value(cards: &Vec<Card>) -> i64 {
@@ -250,7 +268,22 @@ fn straight_value(cards: &Vec<Card>) -> i64 {
 }
 
 fn set_value(cards: &Vec<Card>) -> i64 {
-    get_highest_n_of_kind(cards, 3) as i64
+    let set = get_highest_n_of_kind(cards, 3);
+    if set == Rank::Null {
+        return -1;
+    }
+    let mut total = set as i64 * 13_i64.pow(2);
+
+    let mut cards_sorted = cards.clone();
+    cards_sorted.sort_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap());
+    for (idx, card) in cards[0..2].iter().enumerate() {
+        if card.rank == set {
+            continue;
+        } else {
+            total += 13_i64.pow(1 - idx as u32) * card.rank as i64;
+        }
+    }
+    total
 }
 
 fn two_pair_value(cards: &Vec<Card>) -> i64 {
@@ -259,15 +292,71 @@ fn two_pair_value(cards: &Vec<Card>) -> i64 {
         values.iter().filter(|(_, count)| **count == 2).unzip();
     pairs.sort();
     pairs.reverse();
+    let top_card: Rank = cards
+        .clone()
+        .iter()
+        .filter(|a| a.rank != *pairs[0] && a.rank != *pairs[1])
+        .max_by(|a, b| a.rank.partial_cmp(&b.rank).unwrap())
+        .unwrap()
+        .rank;
+
     if pairs.len() < 2 {
         -1
     } else {
-        (*pairs[0] as i64) * 13 + (*pairs[1] as i64)
+        (*pairs[0] as i64) * 13 * 13 + (*pairs[1] as i64) * 13 + top_card as i64
     }
 }
 
 fn pair_value(cards: &Vec<Card>) -> i64 {
-    return get_highest_n_of_kind(cards, 2) as i64;
+    let pair = get_highest_n_of_kind(cards, 2);
+    if pair == Rank::Null {
+        return -1;
+    }
+
+    let mut cards_sorted = cards.clone();
+    cards_sorted.sort_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap());
+    let mut total = 13_i64.pow(3) * pair as i64;
+    for (idx, card) in cards.iter().enumerate() {
+        if card.rank == pair {
+            continue;
+        } else {
+            total += 13_i64.pow(2 - idx as u32) * card.rank as i64
+        }
+    }
+    total
+}
+
+fn high_card_value(cards: &Vec<Card>) -> i64 {
+    let mut cards_sorted = cards.clone();
+    cards_sorted.sort_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap());
+    let mut total = 0;
+    for (idx, card) in cards_sorted[0..5].iter().enumerate() {
+        total += 13_i64.pow(4 - idx as u32);
+    }
+    return total;
+}
+
+fn get_hand_value(cards: &Vec<Card>) -> i64 {
+    let valuations: Vec<fn(&Vec<Card>) -> i64> = vec![
+        straight_flush_value,
+        quads_value,
+        full_house_value,
+        flush_value,
+        straight_value,
+        set_value,
+        two_pair_value,
+        pair_value,
+        high_card_value,
+    ];
+
+    let base = 13_i64.pow(5);
+    for (idx, val) in valuations.iter().enumerate() {
+        let value = val(cards);
+        if value != -1 {
+            return base * (valuations.len() - 1 - idx) as i64 + value;
+        }
+    }
+    -1
 }
 
 pub struct PassivePokerPlayer {}
